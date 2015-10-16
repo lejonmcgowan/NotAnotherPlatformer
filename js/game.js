c78d0049d2eeee2b87193c6d17ca1c5a5225d59f
LeJon McGowan <lejonmcgowan@gmail.com>
2015-10-15 22:13:16 -0700

    trying to get examples to compile:

--- js/game.js
+++ js/game.js
@@ -1,46 +1,137 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
+var container, stats;
+			var camera, scene, renderer;
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
+			init();
+			animate();
 
-var scene = new THREE.Scene;
+			function init() {
 
-var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
-var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.rotation.y = Math.PI * 45 / 180;
-scene.add(cube);
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
 
-var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-var testCamera = new THREE.OrthographicCamera(-width / 2, width / 2, -height / 2, 10, 10000);
-camera.position.y = 160;
-camera.position.z = 400;
-camera.lookAt(cube.position);
-//testCamera.lookAt(cube.position);
+				var info = document.createElement( 'div' );
+				info.style.position = 'absolute';
+				info.style.top = '10px';
+				info.style.width = '100%';
+				info.style.textAlign = 'center';
+				info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - orthographic view';
+				container.appendChild( info );
 
-scene.add(testCamera);
+				camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000 );
+				camera.position.x = 200;
+				camera.position.y = 100;
+				camera.position.z = 200;
 
-var skyboxGeometry = new THREE.CubeGeometry(100, 100, 100);
-var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000, side: THREE.BackSide });
-var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+				scene = new THREE.Scene();
 
-scene.add(skybox);
+				// Grid
 
-var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 300, 200);
+				var size = 500, step = 50;
 
-scene.add(pointLight);
+				var geometry = new THREE.Geometry();
 
-var clock = new THREE.Clock;
+				for ( var i = - size; i <= size; i += step ) {
 
-function render() {
-	requestAnimationFrame(render);
-	
-	cube.rotation.y -= clock.getDelta();
-	
-	renderer.render(scene, testCamera);
-}
+					geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+					geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
 
-render();
+					geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+					geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+
+				}
+
+				var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+
+				var line = new THREE.LineSegments( geometry, material );
+				scene.add( line );
+
+				// Cubes
+
+				var geometry = new THREE.BoxGeometry( 50, 50, 50 );
+				var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
+
+				for ( var i = 0; i < 100; i ++ ) {
+
+					var cube = new THREE.Mesh( geometry, material );
+
+					cube.scale.y = Math.floor( Math.random() * 2 + 1 );
+
+					cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+					cube.position.y = ( cube.scale.y * 50 ) / 2;
+					cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+
+					scene.add( cube );
+
+				}
+
+				// Lights
+
+				var ambientLight = new THREE.AmbientLight( Math.random() * 0x10 );
+				scene.add( ambientLight );
+
+				var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+				directionalLight.position.x = Math.random() - 0.5;
+				directionalLight.position.y = Math.random() - 0.5;
+				directionalLight.position.z = Math.random() - 0.5;
+				directionalLight.position.normalize();
+				scene.add( directionalLight );
+
+				var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+				directionalLight.position.x = Math.random() - 0.5;
+				directionalLight.position.y = Math.random() - 0.5;
+				directionalLight.position.z = Math.random() - 0.5;
+				directionalLight.position.normalize();
+				scene.add( directionalLight );
+
+				renderer = new THREE.CanvasRenderer();
+				renderer.setClearColor( 0xf0f0f0 );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				stats = new Stats();
+				stats.domElement.style.position = 'absolute';
+				stats.domElement.style.top = '0px';
+				container.appendChild( stats.domElement );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function onWindowResize() {
+
+				camera.left = window.innerWidth / - 2;
+				camera.right = window.innerWidth / 2;
+				camera.top = window.innerHeight / 2;
+				camera.bottom = window.innerHeight / - 2;
+
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			//
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				stats.update();
+
+			}
+
+			function render() {
+
+				var timer = Date.now() * 0.0001;
+
+				camera.position.x = Math.cos( timer ) * 200;
+				camera.position.z = Math.sin( timer ) * 200;
+				camera.lookAt( scene.position );
+
+				renderer.render( scene, camera );
+
+			}
