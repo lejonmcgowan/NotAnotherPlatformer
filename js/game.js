7a1f6a27d9d5b6160733121c8ada981422d3bda2
LeJon McGowan <lejonmcgowan@gmail.com>
2015-10-19 22:20:36 -0700

    manually insert changes to game.js
    no idea why Work comp repo hates me. will merge this was

--- js/game.js
+++ js/game.js
@@ -1,137 +1,84 @@
 var container, stats;
-			var camera, scene, renderer;
+var camera, scene, renderer;
+var width = 640, height = 480;
 
-			init();
-			animate();
+init();
+animate();
 
-			function init() {
+function init() {
 
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-				var info = document.createElement( 'div' );
-				info.style.position = 'absolute';
-				info.style.top = '10px';
-				info.style.width = '100%';
-				info.style.textAlign = 'center';
-				info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - orthographic view';
-				container.appendChild( info );
+	camera = new THREE.OrthographicCamera( -width, width, height, -height, -1000, 2000);
+	camera.position.x = 200;
+	camera.position.y = 100;
+	camera.position.z = 200;
 
-				camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000 );
-				camera.position.x = 200;
-				camera.position.y = 100;
-				camera.position.z = 200;
+	scene = new THREE.Scene();
 
-				scene = new THREE.Scene();
+	/*alternative grid?*/
+	var helper = new THREE.GridHelper( 500, 50 );
+	helper.setColors( 0x000000, 0x808080 );
+	scene.add( helper );
 
-				// Grid
+	// Cube in random grid spot. OO this entire setup later
 
-				var size = 500, step = 50;
+	var geometry = new THREE.BoxGeometry( 100, 100, 100);
+	//var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
+	var material = new THREE.MeshPhongMaterial({color: 0x880000});
 
-				var geometry = new THREE.Geometry();
+	var cube = new THREE.Mesh( geometry, material );
 
-				for ( var i = - size; i <= size; i += step ) {
+	//cube.scale.y = Math.floor( Math.random() * 2 + 1 );
 
-					geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
-					geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+	cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50;
+	cube.position.y = ( cube.scale.y * 50 ) / 2 + 25;
+	cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50;
 
-					geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
-					geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+	scene.add( cube );
 
-				}
+	// Lights
 
-				var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+	var ambientLight = new THREE.AmbientLight(0xFFFFFF);
+	scene.add( ambientLight );
 
-				var line = new THREE.LineSegments( geometry, material );
-				scene.add( line );
+	var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+	directionalLight.position.x = Math.random() - 0.5;
+	directionalLight.position.y = Math.random() - 0.5;
+	directionalLight.position.z = Math.random() - 0.5;
+	directionalLight.position.normalize();
+	scene.add( directionalLight );
 
-				// Cubes
+	var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+	directionalLight.position.x = Math.random() - 0.5;
+	directionalLight.position.y = Math.random() - 0.5;
+	directionalLight.position.z = Math.random() - 0.5;
+	directionalLight.position.normalize();
+	scene.add( directionalLight );
 
-				var geometry = new THREE.BoxGeometry( 50, 50, 50 );
-				var material = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setClearColor( 0xf0f0f0 );
+	//renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize(width, height);
+	container.appendChild( renderer.domElement );
+}
 
-				for ( var i = 0; i < 100; i ++ ) {
+function animate() {
 
-					var cube = new THREE.Mesh( geometry, material );
+	requestAnimationFrame( animate );
 
-					cube.scale.y = Math.floor( Math.random() * 2 + 1 );
+	render();
+}
 
-					cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-					cube.position.y = ( cube.scale.y * 50 ) / 2;
-					cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+function render() {
 
-					scene.add( cube );
+	var timer = Date.now() * 0.0002;
+	//change to manual control for later
+	camera.position.x = Math.cos( timer ) * 200;
+	camera.position.z = Math.sin( timer ) * 200;
+	camera.lookAt( scene.position );
 
-				}
+	renderer.render( scene, camera );
 
-				// Lights
-
-				var ambientLight = new THREE.AmbientLight( Math.random() * 0x10 );
-				scene.add( ambientLight );
-
-				var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
-				directionalLight.position.x = Math.random() - 0.5;
-				directionalLight.position.y = Math.random() - 0.5;
-				directionalLight.position.z = Math.random() - 0.5;
-				directionalLight.position.normalize();
-				scene.add( directionalLight );
-
-				var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
-				directionalLight.position.x = Math.random() - 0.5;
-				directionalLight.position.y = Math.random() - 0.5;
-				directionalLight.position.z = Math.random() - 0.5;
-				directionalLight.position.normalize();
-				scene.add( directionalLight );
-
-				renderer = new THREE.CanvasRenderer();
-				renderer.setClearColor( 0xf0f0f0 );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
-
-				stats = new Stats();
-				stats.domElement.style.position = 'absolute';
-				stats.domElement.style.top = '0px';
-				container.appendChild( stats.domElement );
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize, false );
-
-			}
-
-			function onWindowResize() {
-
-				camera.left = window.innerWidth / - 2;
-				camera.right = window.innerWidth / 2;
-				camera.top = window.innerHeight / 2;
-				camera.bottom = window.innerHeight / - 2;
-
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			}
-
-			//
-
-			function animate() {
-
-				requestAnimationFrame( animate );
-
-				render();
-				stats.update();
-
-			}
-
-			function render() {
-
-				var timer = Date.now() * 0.0001;
-
-				camera.position.x = Math.cos( timer ) * 200;
-				camera.position.z = Math.sin( timer ) * 200;
-				camera.lookAt( scene.position );
-
-				renderer.render( scene, camera );
-
-			}
+}
